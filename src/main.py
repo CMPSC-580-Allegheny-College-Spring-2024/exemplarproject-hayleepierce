290c5f2a57d03dae55f84f2ec109adc983c8cb20
hayleepierce <pierce03@allegheny.edu>
2024-02-15 16:09:19 -0500

    Add documentation & use ruff format fix

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
+"""Main.py file for the Corpus Search program."""
+
 import streamlit as st
-# import nltk
-# from nltk.sentiment import SentimentIntensityAnalyzer
 from development import develop_search_lists, develop_corpus
 from printing import print_top_articles
 from search import search_corpus
 
-st.title('Corpus Search')
+# potential implementation of sentiment anaylysis
+# import nltk
+# from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+st.title("Corpus Search")
 # location of corpus directory
-directory = 'data\corpus'
-search = st.text_input('What would you like to search for?')
+directory = "data\corpus"
+search = st.text_input("What would you like to search for?")
 
 # develop data structures
 search_lists = develop_search_lists(search)
@@ -19,11 +24,11 @@ found_articles = search_corpus(corpus, search_lists)
 
 # if a search was entered print the results
 if type(search) != type(None):
-    st.write('Number of article found:', len(found_articles))
-    percentage = str(int(len(found_articles) / len(corpus) * 100)) + '%'
-    st.write('Percentage of total corpus:', percentage)
+    st.write("Number of article found:", len(found_articles))
+    percentage = str(int(len(found_articles) / len(corpus) * 100)) + "%"
+    st.write("Percentage of total corpus:", percentage)
     # st.write('Positivity percentage:', pos_percentage)
     # st.write('Negativity percentage:', neg_percentage)
 
-    st.header('Top Articles Found', divider='gray')
+    st.header("Top Articles Found", divider="gray")
     print_top_articles(found_articles)
