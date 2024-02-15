0192af178daac24b76789ac2488b7fd148b41b48
hayleepierce <pierce03@allegheny.edu>
2024-02-15 16:05:07 -0500

    Add documentation & use search module

--- src/main.py
+++ src/main.py
@@ -3,36 +3,21 @@ import streamlit as st
 # from nltk.sentiment import SentimentIntensityAnalyzer
 from development import develop_search_lists, develop_corpus
 from printing import print_top_articles
+from search import search_corpus
 
-# sia = SentimentIntensityAnalyzer()
-st.title('Corpus Searcher')
-# st.text_input
+st.title('Corpus Search')
+# location of corpus directory
 directory = 'data\corpus'
 search = st.text_input('What would you like to search for?')
-# search = "do mammalian cells exist?"
 
+# develop data structures
 search_lists = develop_search_lists(search)
 corpus = develop_corpus(directory)
 
-def find_all(list, string):
-    for element in list:
-        if element not in string:
-            return False
-    return True
+# search through the corpus for search_lists
+found_articles = search_corpus(corpus, search_lists)
 
-found_articles = []
-for search_list in search_lists:
-    for article in corpus:
-        for paragraph in article['Content']:
-            if type(paragraph) != type(None):
-                if search_list == search_lists[-1]:
-                    if find_all(search_list, paragraph) and article not in found_articles:
-                        found_articles.append(article)
-                else:
-                    for phrase in search_list:
-                        if phrase in paragraph and article not in found_articles:
-                            found_articles.append(article)
-
+# if a search was entered print the results
 if type(search) != type(None):
     st.write('Number of article found:', len(found_articles))
     percentage = str(int(len(found_articles) / len(corpus) * 100)) + '%'
